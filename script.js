0897cc12f3c83ff6fc7e2ced77b5f03a5563ca51
Zoey VanderMeer <owenvandermeer@gmail.com>
2024-09-05 20:59:11 -0400

    Random corner colour generation now ensures colours aren't too similar

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// import JSConfetti from 'js-confetti'
-
 const jsConfetti = new JSConfetti()
 
 let draggedTile = null;
@@ -8,10 +6,7 @@ let placeholderTile = null;
 let randomize = true;
 let puzzleSolved = false;
 
-let color1 = null;
-let color2 = null;
-let color3 = null;
-let color4 = null;
+const cornerColors = [];
 
 let fixedTileNumList = null;
 
@@ -24,6 +19,8 @@ let timerRunning = false;
 let timerSeconds = 0;
 
 let tileToBeStopped = false;
+
+const colorSimilarityThreshold = 9;
 
 // Add listeners to report constant cursor position
 document.addEventListener('touchmove', onCursorMove);
@@ -56,6 +53,49 @@ solutionButton.addEventListener('click', () => {
     }
     generateGrid();
 });
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function deltaE(rgbA, rgbB) {
+    let labA = rgb2lab(rgbA);
+    let labB = rgb2lab(rgbB);
+    let deltaL = labA[0] - labB[0];
+    let deltaA = labA[1] - labB[1];
+    let deltaB = labA[2] - labB[2];
+    let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    let deltaC = c1 - c2;
+    let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    let sc = 1.0 + 0.045 * c1;
+    let sh = 1.0 + 0.015 * c1;
+    let deltaLKlsl = deltaL / (1.0);
+    let deltaCkcsc = deltaC / (sc);
+    let deltaHkhsh = deltaH / (sh);
+    let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+    return i < 0 ? 0 : Math.sqrt(i);
+}
+  
+function rgb2lab(rgb){
+    let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+    x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+    y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+    z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+}
 
 // Helper sleep function
 function sleep(ms) {
@@ -245,11 +285,27 @@ async function stopDrag(touch) {
 
 // Selects random colors for the corners which will be used to generate the gradient
 function randomizeCornerColors() {
-    // Define the four corner colors
-    color1 = getRandomColor();
-    color2 = getRandomColor();
-    color3 = getRandomColor();
-    color4 = getRandomColor();
+    let suitableColors = false;
+
+    while(!suitableColors) {
+        const colorDeltas = [];
+
+        for(let i = 0; i < 4; i++) {
+            cornerColors[i] = getRandomColor();
+        }
+
+        for(let i = 0; i < 4; i++) {
+            let rgb1 = hexToRgb(cornerColors[i])
+            for(let j = 1+i; j < 4; j++) {
+                let rgb2 = hexToRgb(cornerColors[j]);
+                colorDeltas.push(deltaE([rgb1.r, rgb1.g, rgb1.b], [rgb2.r, rgb2.g, rgb2.b]));
+            }
+        }
+
+        if(Math.min(...colorDeltas) > colorSimilarityThreshold) {
+            suitableColors = true;
+        }
+    }
 }
 
 // Generate a random hex code
@@ -488,7 +544,7 @@ function generateGrid() {
     }
 
     // Generate gradient colors for grid
-    let colorGrid = generateGradientGrid(color1, color2, color3, color4, columns, rows);
+    let colorGrid = generateGradientGrid(cornerColors[0], cornerColors[1], cornerColors[2], cornerColors[3], columns, rows);
 
     // Create fixed tile pattern, setting corners as fixed
     if(randomize) {
